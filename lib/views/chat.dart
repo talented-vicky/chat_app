495a941c60201fe205bf6a1b32738ad2a9e8ac74
talented_vicky <victorotubure7@gmail.com>
2024-04-10 15:57:20 +0100

    created widgets for signup and chat sections

---
+++ lib/views/chat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('stuff her')),
+      body: Center(
+        child: Text('random main things here'),
+      ),
+    );
+  }
+}
